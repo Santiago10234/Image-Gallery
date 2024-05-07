94f43b16d8daa97d82ccd40dad07ecd578441f47
santiago10234 <ariza.ps4@gmail.com>
2024-05-07 15:48:33 -0600

    Creacion del modo oscuro (en proceso)

--- gallery.js
+++ gallery.js
@@ -9,3 +9,10 @@ function abrirFulImg(reference){
 function cerrarImg() {
     fulImgBox.style.display = "none"
 }
+
+const btnSwitch = document.getElementById("switch")
+
+btnSwitch.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+})
