fff06c8940cf0caf654ba090ed2a62f93703a132
santiago10234 <ariza.ps4@gmail.com>
2024-05-08 11:09:43 -0600

    Mode dark y navbar terminados

--- gallery.js
+++ gallery.js
@@ -15,4 +15,21 @@ const btnSwitch = document.getElementById("switch")
 btnSwitch.addEventListener("click", function () {
     document.body.classList.toggle("dark");
     btnSwitch.classList.toggle("active");
-})
+
+    // Guardar el modo en localstorage.
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark-mode","true")
+    } else {
+        localStorage.setItem("dark-mode", "false")
+    }
+
+});
+
+// Obtener el modo actual.
+if(localStorage.getItem("dark-mode") === "true"){
+    document.body.classList.add("dark");
+    btnSwitch.classList.add("active");
+} else {
+    document.body.classList.remove("dark");
+    btnSwitch.classList.remove("active");
+}
